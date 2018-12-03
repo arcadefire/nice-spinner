66f83cd72c0eabeece44773bf2cc9a05dc2b7fcf
Angelo Marchesin <marchesinangelo@gmail.com>
2018-12-03 17:52:30 +0100

    Include sample-app (#100)
    * Edit the project's structure to include a sample app
    * Add some layout
    * Some gradle bump-ups

---
+++ app/src/main/java/org/angmarc/app/MainActivity.java
@@ -0,0 +1,26 @@
+package org.angmarc.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.angmarch.views.NiceSpinner;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        NiceSpinner niceSpinner = findViewById(R.id.nice_spinner);
+        List<String> dataset = new LinkedList<>(Arrays.asList("One", "Two", "Three", "Four", "Five"));
+        niceSpinner.attachDataSource(dataset);
+
+        NiceSpinner tintedSpinner = findViewById(R.id.tinted_nice_spinner);
+        tintedSpinner.attachDataSource(dataset);
+    }
+}
